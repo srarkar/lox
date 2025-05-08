e37275faa86b7abf2a0b019e4a42e5952c8d7a66
srarkar <rickdsarkar@gmail.com>
2025-05-08 19:32:39 -0400

    added Scanner.java. removed public modifiers

--- Token.java
+++ Token.java
@@ -1,6 +1,6 @@
 package com.craftinginterpreters.lox;
 
-public class Token {
+class Token {
     final TokenType type;
     final String lexeme;
     final Object literal;
--- TokenType.java
+++ TokenType.java
@@ -1,4 +1,4 @@
-public enum TokenType {
+enum TokenType {
     // Single-character tokens.
     LEFT_PAREN, RIGHT_PAREN, LEFT_BRACE, RIGHT_BRACE,
     COMMA, DOT, MINUS, PLUS, SEMICOLON, SLASH, STAR,
