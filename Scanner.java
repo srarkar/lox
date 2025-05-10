de666372dec8aba37339103ed037d7e7ede50f74
srarkar <rickdsarkar@gmail.com>
2025-05-10 13:07:02 -0400

    additional Scanner functionality

--- Scanner.java
+++ Scanner.java
@@ -47,7 +47,44 @@ List<Token> scanTokens() {
       case '+': addToken(PLUS); break;
       case ';': addToken(SEMICOLON); break;
       case '*': addToken(STAR); break; 
+
+      // add additional logic since !=, <=, etc. must be handled as a single lexeme
+      case '!':
+        addToken(match('=') ? BANG_EQUAL : BANG);
+        break;
+      case '=':
+        addToken(match('=') ? EQUAL_EQUAL : EQUAL);
+        break;
+      case '<':
+        addToken(match('=') ? LESS_EQUAL : LESS);
+        break;
+      case '>':
+        addToken(match('=') ? GREATER_EQUAL : GREATER);
+        break;
+
+      case '/':
+        if (match('/')) {
+          // A comment goes until the end of the line.
+          while (peek() != '\n' && !isAtEnd()) advance();
+        } else {
+          addToken(SLASH);
+        }
+        break;
+      
+      // handle characters that Lox doesn't recognize
+      default:
+        Lox.error(line, "Unexpected character.");
+        break;
     }
+  }
+
+  // conditional advance if current matches expected
+  private boolean match(char expected) {
+    if (isAtEnd()) return false;
+    if (source.charAt(current) != expected) return false;
+
+    current++;
+    return true;
   }
 
   // Helpers
