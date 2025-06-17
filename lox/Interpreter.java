df7bc9776aa98bde1099be46c0056c29f12d50ba
srarkar <rickdsarkar@gmail.com>
2025-06-16 21:33:07 -0400

    beginning runtime error handling

--- lox/Interpreter.java
+++ lox/Interpreter.java
@@ -13,6 +13,44 @@ class Interpreter implements Expr.Visitor<Object> {
         return expr.accept(this);
     }
 
+    // binary operators
+    @Override
+    public Object visitBinaryExpr(Expr.Binary expr) {
+      Object left = evaluate(expr.left);
+      Object right = evaluate(expr.right); 
+  
+      switch (expr.operator.type) {
+        case GREATER:
+          return (double)left > (double)right;
+        case GREATER_EQUAL:
+          return (double)left >= (double)right;
+        case LESS:
+          return (double)left < (double)right;
+        case LESS_EQUAL:
+          return (double)left <= (double)right;
+        case MINUS:
+          return (double)left - (double)right;
+        case PLUS:
+          if (left instanceof Double && right instanceof Double) {
+            return (double)left + (double)right;
+          } 
+
+          if (left instanceof String && right instanceof String) {
+            return (String)left + (String)right;
+          }
+          break;
+        case BANG_EQUAL: return !isEqual(left, right);
+        case EQUAL_EQUAL: return isEqual(left, right);
+        case SLASH:
+          return (double)left / (double)right;
+        case STAR:
+          return (double)left * (double)right;
+      }
+  
+      // Unreachable.
+      return null;
+    }
+
     // convert tree node into runtime value
     @Override
     public Object visitLiteralExpr(Expr.Literal expr) {
@@ -26,11 +64,35 @@ class Interpreter implements Expr.Visitor<Object> {
       Object right = evaluate(expr.right);
   
       switch (expr.operator.type) {
+        case BANG:
+            return !isTruthy(right);
         case MINUS:
+          checkNumberOperand(expr.operator, right); // verify object type
           return -(double)right;
       }
   
       // Unreachable.
       return null;
+    }
+
+    private void checkNumberOperand(Token operator, Object operand) {
+      if (operand instanceof Double) return;
+      throw new RuntimeError(operator, "Operand must be a number.");
+    }
+    
+    // only false and nil are falsey
+    private boolean isTruthy(Object object) {
+        if (object == null) return false;
+        if (object instanceof Boolean) return (boolean)object;
+        return true;
+      }
+    
+    // it is important to consider what "equality" means in the context of a language
+    // prevent null pointer exception
+    private boolean isEqual(Object a, Object b) {
+      if (a == null && b == null) return true;
+      if (a == null) return false;
+
+      return a.equals(b);
     }
 }
